7f0044bdf48f4fc21e7edb11bb3ae8ce94e0d819
HieuKyNang <bmdinhhieu@gmail.com>
2025-05-03 08:49:47 +0700

    fix add item

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -4,7 +4,7 @@ import axios from 'axios'
 import { backendUrl } from '../App'
 import { toast } from 'react-toastify'
 
-const Add = ({token}) => {
+const Add = ({ token }) => {
 
   const [image1, setImage1] = useState(false);
   const [image2, setImage2] = useState(false);
@@ -14,23 +14,21 @@ const Add = ({token}) => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
-  const [category, setCategory] = useState("Men");
-  const [subCategory, setSubCategory] = useState("Topwear");
+  const [category, setCategory] = useState("Lifstick");
+  const [brand, setBrand] = useState("Dior");
   const [bestseller, setBestseller] = useState(false);
-  const [sizes, setSizes] = useState([]);
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
-    try{
+    try {
 
       const formData = new FormData();
 
       formData.append("name", name);
       formData.append("description", description);
       formData.append("price", price);
-      formData.append("sizes", JSON.stringify(sizes));
       formData.append("category", category);
-      formData.append("subCategory", subCategory);
+      formData.append("brand", brand);
       formData.append("bestseller", bestseller);
 
       image1 && formData.append("image1", image1);
@@ -38,9 +36,9 @@ const Add = ({token}) => {
       image3 && formData.append("image3", image3);
       image4 && formData.append("image4", image4);
 
-      const response = await axios.post(backendUrl + "/api/product/add", formData,{headers:{token}});
-      
-      if(response.data.success){
+      const response = await axios.post(backendUrl + "/api/product/add", formData, { headers: { token } });
+
+      if (response.data.success) {
         toast.success(response.data.message);
         setName('');
         setDescription('')
@@ -50,7 +48,7 @@ const Add = ({token}) => {
         setImage4(false);
         setPrice('');
       }
-      else{
+      else {
         toast.error(response.data.message);
       }
 
@@ -69,40 +67,40 @@ const Add = ({token}) => {
         <div className='flex gap-2'>
           <label htmlFor='image1'>
             <img className='w-20' src={!image1 ? assets.upload_area : URL.createObjectURL(image1)} alt='' />
-            <input onChange={(e)=>setImage1(e.target.files[0])} type='file' id='image1' hidden/>
+            <input onChange={(e) => setImage1(e.target.files[0])} type='file' id='image1' hidden />
           </label>
 
           <label htmlFor='image2'>
             <img className='w-20' src={!image2 ? assets.upload_area : URL.createObjectURL(image2)} alt='' />
-            <input onChange={(e)=>setImage2(e.target.files[0])} type='file' id='image2' hidden/>
+            <input onChange={(e) => setImage2(e.target.files[0])} type='file' id='image2' hidden />
           </label>
 
           <label htmlFor='image3'>
             <img className='w-20' src={!image3 ? assets.upload_area : URL.createObjectURL(image3)} alt='' />
-            <input onChange={(e)=>setImage3(e.target.files[0])} type='file' id='image3' hidden/>
+            <input onChange={(e) => setImage3(e.target.files[0])} type='file' id='image3' hidden />
           </label>
 
           <label htmlFor='image4'>
             <img className='w-20' src={!image4 ? assets.upload_area : URL.createObjectURL(image4)} alt='' />
-            <input onChange={(e)=>setImage4(e.target.files[0])} type='file' id='image4' hidden/>
+            <input onChange={(e) => setImage4(e.target.files[0])} type='file' id='image4' hidden />
           </label>
         </div>
       </div>
-      
+
       <div>
         <p className='w-full'>Product Name</p>
-        <input onChange={(e)=>setName(e.target.value)} value={name} className='w-full max-w-[500px] px-3 py-2' type='text' placeholder='Type here' required />
+        <input onChange={(e) => setName(e.target.value)} value={name} className='w-full max-w-[500px] px-3 py-2' type='text' placeholder='Type here' required />
       </div>
 
       <div>
         <p className='w-full'>Product Description</p>
-        <input onChange={(e)=>setDescription(e.target.value)} value={description} className='w-full max-w-[500px] px-3 py-2' type='text' placeholder='Write content here' required />
+        <input onChange={(e) => setDescription(e.target.value)} value={description} className='w-full max-w-[500px] px-3 py-2' type='text' placeholder='Write content here' required />
       </div>
 
       <div className='flex flex-col sm:flex-row gap-2 w-full sm:gap-8'>
         <div>
           <p className='mb-2'>Product Category</p>
-          <select onChange={(e)=>setCategory(e.target.value)} className='w-full px-3 py-2'>
+          <select onChange={(e) => setCategory(e.target.value)} className='w-full px-3 py-2'>
             <option value='Men'>Men</option>
             <option value='Women'>Women</option>
             <option value='Kids'>Kids</option>
@@ -110,8 +108,8 @@ const Add = ({token}) => {
         </div>
 
         <div>
-          <p className='mb-2'>Sub Category</p>
-          <select onChange={(e)=>setSubCategory(e.target.value)} className='w-full px-3 py-2'>
+          <p className='mb-2'>Brand</p>
+          <select onChange={(e) => setBrand(e.target.value)} className='w-full px-3 py-2'>
             <option value='Topwear'>Topwear</option>
             <option value='Bottomwear'>Bottomwear</option>
             <option value='Winterwear'>Winterwear</option>
@@ -120,11 +118,11 @@ const Add = ({token}) => {
 
         <div>
           <p className='mb-2'>Product Price</p>
-          <input onChange={(e)=>setPrice(e.target.value)} value={price} className='w-full px-3 py-2 sm:w-[120px]' type='number' placeholder='25' />
+          <input onChange={(e) => setPrice(e.target.value)} value={price} className='w-full px-3 py-2 sm:w-[120px]' type='number' placeholder='25' />
         </div>
       </div>
 
-      <div>
+      {/* <div>
         <p className='mb-2'>Product Sizes</p>
         <div className='flex gap-3'>
 
@@ -149,10 +147,10 @@ const Add = ({token}) => {
           </div>
 
         </div>
-      </div>
+      </div> */}
 
       <div className='flex gap-2 mt-2'>
-        <input onChange={()=>setBestseller(prev => !prev)} checked={bestseller} type='checkbox' id='bestseller' />
+        <input onChange={() => setBestseller(prev => !prev)} checked={bestseller} type='checkbox' id='bestseller' />
         <label className='cursor-pointer' htmlFor='bestseller'>Add to bestseller</label>
       </div>
 
